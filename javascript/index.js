b80acfa93c08551ed1dc4137f82433ea93681f80
Vedant Jadhav <60446303+vedantjdv@users.noreply.github.com>
2021-08-03 19:33:34 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -65,9 +65,9 @@ axios.request(options).then(function (response) {
           displayMode: 'regions',
           colorAxis: {colors: ['#e5ef88', '#d4b114', '#e85a03','#ff3333', '#e60000', '#980000', '#4d0000']},
           resolution: 'provinces',
-          // backgroundColor: '#81d4fa',
+          // backgroundColor: '#81',
           datalessRegionColor: '#ffffff',
-          defaultColor: '#f5f5f5',
+          defaultColor: '#ffffff',
           width: 640,
           height: 480
         };
